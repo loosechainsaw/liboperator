a4fc06155056afecd0a4b068669447cda8f36c25
loosechainsaw <blair.joel.davidson@gmail.com>
2013-06-13 21:05:43 +0800

    Remove need for public copy ctor, default ctor.

--- liboperator/liboperator/has_addition_operator.h
+++ liboperator/liboperator/has_addition_operator.h
@@ -8,23 +8,23 @@ namespace liboperator
 	class has_addition_operator
 	{
 	private:
-	
+
 		typedef char no;
-	
+
 		template<typename X1, typename X2>
-		static auto has(X1 a, X2 b) -> decltype( a + b){
-	
+		static auto has(X1&& a, X2&& b) -> decltype( a + b){
+
 		}
-	
+
 		static no has(...){
-	
+
 		}
-	
+
 	public:
 		enum{
 			value = ( !std::is_same<decltype( has(std::declval<T1>(), std::declval<T2>())),no>::value)
 		};
-	
+
 	};
 
 
