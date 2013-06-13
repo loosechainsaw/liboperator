c599e9300511975bfe75f25d36ce9e565eb5b86a
loosechainsaw <blair.joel.davidson@gmail.com>
2013-06-13 20:50:14 +0800

    Update has_addition_operator.h

--- liboperator/liboperator/has_addition_operator.h
+++ liboperator/liboperator/has_addition_operator.h
@@ -1,4 +1,3 @@
-
 #ifndef HAS_ADDITION_OPERATOR
 #define HAS_ADDITION_OPERATOR 
 
@@ -9,33 +8,23 @@ namespace liboperator
 	class has_addition_operator
 	{
 	private:
-
+	
 		typedef char no;
-
+	
 		template<typename X1, typename X2>
 		static auto has(X1 a, X2 b) -> decltype( a + b){
-
+	
 		}
-
+	
 		static no has(...){
-
+	
 		}
-
+	
 	public:
 		enum{
-			value = (sizeof( has(T1(), T2())) != sizeof(no))
+			value = ( !std::is_same<decltype( has(std::declval<T1>(), std::declval<T2>())),no>::value)
 		};
-
-	};
-
-	template<>
-	class has_addition_operator<char,char>
-	{
-	public:
-		enum{
-			value = true
-		};
-
+	
 	};
 
 
