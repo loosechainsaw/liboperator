17d70c1df39bd7ae65e759792dab31e6ab1ce1a1
Blair Davidson <blair.joel.davidson@gmail.com>
2013-06-14 10:11:32 +0800

    Completed detecting operator * / -

---
+++ liboperator/liboperator/has_multiplication_operator.h
@@ -0,0 +1,33 @@
+#ifndef HAS_MULTIPLICATION_OPERATOR
+#define HAS_MULTIPLICATION_OPERATOR 
+
+namespace liboperator
+{
+
+	template<class T1, class T2>
+	class has_multiplication_operator
+	{
+	private:
+
+		class no{};
+
+		template<typename X1, typename X2>
+		static auto has(X1&& a, X2&& b) -> decltype( a * b){
+
+		}
+
+		static no has(...){
+
+		}
+
+	public:
+		enum{
+			value = ( !std::is_same<decltype( has(std::declval<T1>(), std::declval<T2>())),no>::value)
+		};
+
+	};
+
+
+}
+
+#endif
--- liboperator/liboperator/liboperator.cpp
+++ liboperator/liboperator/liboperator.cpp
@@ -23,14 +23,6 @@ int main()
 	
 	using namespace std;
 
-	point a;
-	a.x = 5;
-	a.y = 6;
-
-	point b;
-	b.x = 10;
-	b.y = 20;
-
 	cout << "Points can be added together is: " << boolalpha << (liboperator::has_addition_operator<point,point>::value) << endl;
 	cout << "Points can be with characters is: " << boolalpha << (liboperator::has_addition_operator<point,char>::value) << endl;
 
