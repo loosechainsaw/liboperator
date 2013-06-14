629ee21265a434810428e217e990682c020636e1
Blair Davidson <blair.joel.davidson@gmail.com>
2013-06-14 10:28:31 +0800

    Initial work for operator deref

---
+++ liboperator/liboperator/has_dereference_operator.h
@@ -0,0 +1,25 @@
+#ifndef HAS_DEREFERENCE_OPERATOR
+#define HAS_DEREFERENCE_OPERATOR 
+
+namespace liboperator
+{
+
+  template<class T>
+  class has_dereference_operator
+  {
+  private:
+      class no{};
+      template<class X>
+      static auto has(X&& t) -> decltype (*t);
+
+      static no has(...);
+  public:
+      enum {
+          value = ( !std::is_same<decltype(has( std::declval<T>())),no>::value)
+      };
+  };
+
+
+}
+
+#endif
