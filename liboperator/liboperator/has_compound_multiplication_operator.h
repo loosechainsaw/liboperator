6820ba19e8f3133162feee5cab235846709d4084
Blair Davidson <blair.joel.davidson@gmail.com>
2013-06-17 17:01:04 +0800

    Add modulus support

---
+++ liboperator/liboperator/has_compound_multiplication_operator.h
@@ -0,0 +1,35 @@
+#ifndef HAS_COMPOUNDMULTIPLICATION_OPERATOR
+#define HAS_COMPOUNDMULTIPLICATION_OPERATOR 
+
+namespace liboperator
+{
+
+	template<class T1, class T2>
+	class has_compound_multiplication_operator
+	{
+		typedef typename std::remove_pointer< typename std::remove_reference<T1>::type >::type AT1;
+		typedef typename std::remove_pointer< typename std::remove_reference<T2>::type>::type AT2;
+	private:
+
+		class no{};
+
+		template<typename X1, typename X2>
+		static auto has(X1&& a, X2&& b) -> decltype( a *= b){
+
+		}
+
+		static no has(...){
+
+		}
+
+	public:
+		enum{
+			value = ( !std::is_same<decltype( has(std::declval<AT1>(), std::declval<AT2>())),no>::value)
+		};
+
+	};
+
+
+}
+
+#endif
---
+++ liboperator/liboperator/has_modulus_operator.h
@@ -0,0 +1,35 @@
+#ifndef HAS_MODULUS_OPERATOR
+#define HAS_MODULUS_OPERATOR 
+
+namespace liboperator
+{
+
+	template<class T1, class T2>
+	class has_modulus_operator
+	{
+		typedef typename std::remove_pointer< typename std::remove_reference<T1>::type >::type AT1;
+		typedef typename std::remove_pointer< typename std::remove_reference<T2>::type>::type AT2;
+	private:
+
+		class no{};
+
+		template<typename X1, typename X2>
+		static auto has(X1&& a, X2&& b) -> decltype( a % b){
+
+		}
+
+		static no has(...){
+
+		}
+
+	public:
+		enum{
+			value = ( !std::is_same<decltype( has(std::declval<AT1>(), std::declval<AT2>())),no>::value)
+		};
+
+	};
+
+
+}
+
+#endif
